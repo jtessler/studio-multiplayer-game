8c189f8702f57ee11c2002a2f0a59586e5fae97d
Devraj Mehta <devm33@gmail.com>
2018-04-09 00:20:38 -0400

    Add Rock Paper Scissors game

--- src/gameData.js
+++ src/gameData.js
@@ -1,5 +1,6 @@
 import ChatRoom from './ChatRoom.js';
 import TicTacToe from './TicTacToe.js';
+import RockPaperScissors from './RockPaperScissors.js';
 
 const gameData = {
 
@@ -20,6 +21,16 @@ const gameData = {
     maxUsers: 2,
     component: TicTacToe,
   },
+
+  rockpaperscissors: {
+    title: "Rock Paper Scissors",
+    authors: "Devraj Mehta",
+    description: "Class 2-player rock paper scissors",
+    minUsers: 2,
+    maxUsers: 2,
+    component: RockPaperScissors,
+  },
+
 
 }
 
