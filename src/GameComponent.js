332b6f115bdb71ceaf9184894db4626abeb89016
Joe Tessler <jrt@google.com>
2019-03-15 22:33:24 -0400

    Introduce GameComponent subclass
    Designed to encapsulate Firebase data change handling and react-router
    state details. Exposes the following helper methods for all subclasses:
    
      - getMyUserId()
      - getSessionCreatorUserId()
      - getSessionDatabaseRef()
      - getSessionId()
      - getSessionUserIds()
      - onSessionDataChanged(data)
    
    See: #8

---
+++ src/GameComponent.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import firebase from 'firebase';
+
+const SESSION_DATA_PATH = "/session";
+
+export default class GameComponent extends React.Component {
+  /**
+   * Called whenever the session data stored at '/session/<id>/' changes.
+   * Passes said data as the argument. Guaranteed non-null.
+   */
+  onSessionDataChanged(data) {
+    // No-op by default.
+  }
+
+  /**
+   * Returns a Firebase real-time database reference to the current session
+   * data, i.e. '/session/<id>/'.
+   */
+  getSessionDatabaseRef() {
+    return firebase.database()
+        .ref(SESSION_DATA_PATH)
+        .child(this.getSessionId());
+  }
+
+  /** Returns the current session ID as stored in Firebase.  */
+  getSessionId() {
+    return this.props.match.params.id;
+  }
+
+  /** Returns the list of user IDs connected to the current session.  */
+  getSessionUserIds() {
+    return this.props.location.state.users;
+  }
+
+  /** Returns the user ID of the one who created this current session. */
+  getSessionCreatorUserId() {
+    return this.props.location.state.creator;
+  }
+
+  /** Returns the user ID of the current user, i.e. YOU. */
+  getMyUserId() {
+    return firebase.auth().currentUser.uid;
+  }
+
+  componentWillMount() {
+    this.getSessionDatabaseRef().on("value", snapshot => {
+      if (snapshot.val() !== null) {
+        this.onSessionDataChanged(snapshot.val());
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.getSessionDatabaseRef().off();
+  }
+}
