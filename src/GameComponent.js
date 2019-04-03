db66eab6df243d0ec8c218918139be0fd3695a43
Joe Tessler <jrt@google.com>
2019-04-02 22:29:08 -0400

    Add session metadata listener to game component
    Previously we relied solely on the session metadata passed from browser
    history. Now we additionally listen to metadata changes from the
    Firebase database and update state accordingly. Therefore subclasses can
    assume any inherited 'get' calls to access session metadata will be
    up-to-date.
    
    Fixes #22

--- src/GameComponent.js
+++ src/GameComponent.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import firebase from 'firebase';
+import gameData from './gameData.js';
 
 const SESSION_DATA_PATH = "/session";
+const SESSION_METADATA_PATH = "/session-metadata";
 
 export default class GameComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      creator: props.location.state.creator || "",
+      users: props.location.state.users || [],
+      title: props.location.state.title || "Studio Games!",
+    };
+    document.title = this.state.title;
+  }
+
   /**
    * Called whenever the session data stored at '/session/<id>/' changes.
    * Passes said data as the argument. Guaranteed non-null.
    */
   onSessionDataChanged(data) {
+    // No-op by default.
+  }
+
+  /**
+   * Called whenever the session metadata stored at '/session-metadata/<id>/'
+   * changes. Passes said data as the argument. Guaranteed non-null.
+   */
+  onSessionMetadataChanged(metadata) {
     // No-op by default.
   }
 
@@ -22,6 +42,16 @@ export default class GameComponent extends React.Component {
         .child(this.getSessionId());
   }
 
+  /**
+   * Returns a Firebase real-time database reference to the current session
+   * metadata, i.e. '/session-metadata/<id>/'.
+   */
+  getSessionMetadataDatabaseRef() {
+    return firebase.database()
+        .ref(SESSION_METADATA_PATH)
+        .child(this.getSessionId());
+  }
+
   /** Returns the current session ID as stored in Firebase.  */
   getSessionId() {
     return this.props.match.params.id;
@@ -29,37 +59,22 @@ export default class GameComponent extends React.Component {
 
   /** Returns the list of user IDs connected to the current session.  */
   getSessionUserIds() {
-    return this.getSessionState("users");
+    return this.state.users;
   }
 
   /** Returns the user ID of the one who created this current session. */
   getSessionCreatorUserId() {
-    return this.getSessionState("creator");
+    return this.state.creator;
   }
 
   /** Returns the session title, e.g., "Rock, Paper, Scissors" */
   getSessionTitle() {
-    return this.getSessionState("title");
+    return this.state.title;
   }
 
   /** Returns the user ID of the current user, i.e. YOU. */
   getMyUserId() {
     return firebase.auth().currentUser.uid;
-  }
-
-  /**
-   * Returns the session state object from browser history or null if it is
-   * missing.
-   */
-  getSessionState(state) {
-    if (this.props.location.state && state in this.props.location.state) {
-      return this.props.location.state[state];
-    } else {
-      console.warn(
-        "Missing session state. Please go back to the waiting room " +
-        "and reload this game.", state);
-      return null;
-    }
   }
 
   componentWillMount() {
@@ -68,10 +83,27 @@ export default class GameComponent extends React.Component {
         this.onSessionDataChanged(snapshot.val());
       }
     });
-    document.title = this.getSessionTitle() || "Studio Games!";
+
+    this.getSessionMetadataDatabaseRef().on("value", snapshot => {
+      let data = snapshot.val();
+      if (data !== null) {
+        let title = this.state.title;
+        if (data.type in gameData) {
+          title = gameData[data.type].title;
+        }
+        document.title = title;
+        this.setState({
+          creator: data.creator,
+          users: data.users,
+          title: title,
+        });
+        this.onSessionMetadataChanged(data);
+      }
+    });
   }
 
   componentWillUnmount() {
     this.getSessionDatabaseRef().off();
+    this.getSessionMetadataDatabaseRef().off();
   }
 }
